9d0a8b6213ed56a346def632bb308a4c28adb4b6
SvetlanaShin <susperson@gmail.com>
2022-08-24 19:10:58 +0600

    Переменные ч.1. ДЗ 3. Задача 6.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        //Задача 3_6
+        var firstFighterWeight = 78.2;
+        var secondFighterWeight = 82.7;
 
+        var fightersWeight = firstFighterWeight + secondFighterWeight;
+        System.out.println("Общий вес двух бойцов " + fightersWeight + " кг.");
+
+        var weightDifference = secondFighterWeight - firstFighterWeight;
+        System.out.println("Разница в весе бойцов: " + weightDifference + " кг.");
     }
 }
